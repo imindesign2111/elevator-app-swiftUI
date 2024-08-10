9683501fa2b077ba737ed47ed7601a3173a74d4b
nvdesign96 <phanngocvu891@gmail.com>
2024-08-11 01:07:43 +0900

    Initial Commit

---
+++ Elevator-App-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elevator-App-SwiftUI
+//
+//  Created by Phan Ngoc Vu on 2024/08/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elevator-App-SwiftUI/Elevator_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elevator_App_SwiftUIApp.swift
+//  Elevator-App-SwiftUI
+//
+//  Created by Phan Ngoc Vu on 2024/08/11.
+//
+
+import SwiftUI
+
+@main
+struct Elevator_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
